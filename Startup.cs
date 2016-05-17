22e309e820f7b9e363e80f748175277375f44085
Matías Quaranta <ealsur@hotmail.com>
2016-05-17 00:21:32 -0300

    migrating to rc2

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,6 @@
-using Microsoft.AspNet.Builder;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 
@@ -16,14 +18,22 @@ namespace MVPStream
         {
             app.UseStaticFiles();
             app.UseStatusCodePagesWithRedirects("~/error/error{0}");
-            //  app.UseDeveloperExceptionPage();
-
              app.UseMvc(routes =>
              {
                          routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
              });
         }
         
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
     }
 }
 
