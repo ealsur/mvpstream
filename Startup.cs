944244c07a04a2561dec6eccc45a174d91dd2f5b
Matías Quaranta <ealsur@hotmail.com>
2016-06-27 15:17:09 -0300

    Checking again

--- Startup.cs
+++ Startup.cs
@@ -33,6 +33,7 @@ namespace MVPStream
         {
             app.UseStaticFiles();
             app.UseStatusCodePagesWithRedirects("~/error/error{0}");
+            app.UseDeveloperExceptionPage();
             app.UseMvc(routes =>
             {
                         routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
