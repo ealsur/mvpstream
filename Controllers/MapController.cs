0e2c24a4bebb00e23b0c51b8121f42104d5ab8eb
ealsur <ealsur@hotmail.com>
2016-11-16 17:31:27 -0300

    Temporary fix

--- Controllers/MapController.cs
+++ Controllers/MapController.cs
@@ -40,57 +40,57 @@ namespace MVPStream.Controllers
         [ResponseCache(Duration = 43200)]
         public IActionResult Feed()
         {
-            using(var stream = new System.IO.MemoryStream())
-            {
-            const string baseUrl = "http://mvpstream.azurewebsites.net/";
-            var model = HomeService.GetModel(_searchService);
-            var TextWriter = new XmlTextWriter(stream, Encoding.UTF8);
-            TextWriter.WriteStartDocument();
-        
-            //Below tags are mandatory rss tag
-            TextWriter.WriteStartElement("rss");
-            TextWriter.WriteAttributeString("version", "2.0");
-        
-            // Channel tag will contain RSS feed details
-            TextWriter.WriteStartElement("channel");
-            TextWriter.WriteElementString("title", "MVPStream");
-            TextWriter.WriteElementString("description", "MVPStream RSS Feed");
-            TextWriter.WriteElementString("link", baseUrl);
-            
-            foreach(var item in model.UltimasPublicaciones)
-            {
-                TextWriter.WriteStartElement("item");
-                TextWriter.WriteElementString("title", item.Titulo);
-                TextWriter.WriteElementString("description", item.Descripcion);
-                TextWriter.WriteElementString("link", item.Url);
-                foreach(var tag in item.Tags){
-                    TextWriter.WriteElementString("category", tag);
-                }
-                TextWriter.WriteEndElement();
-            }
-            foreach(var item in model.UltimosVideos)
-            {
-                TextWriter.WriteStartElement("item");
-                TextWriter.WriteElementString("title", item.Titulo);
-                TextWriter.WriteElementString("description", item.Descripcion);
-                TextWriter.WriteElementString("link", item.Url);
-                foreach(var tag in item.Tags){
-                    TextWriter.WriteElementString("category", tag);
-                }
-                TextWriter.WriteEndElement();
-            }
-            TextWriter.WriteEndElement();
-            TextWriter.WriteEndElement();
-            TextWriter.WriteEndDocument();
-            TextWriter.Flush();
-            stream.Position=0;
-            using(var streamReader = new System.IO.StreamReader(stream))
-            {
-                
-                return Content(streamReader.ReadToEnd(), "application/rss+xml");    
-            }
-            }
-            
+			//using(var stream = new System.IO.MemoryStream())
+			//{
+			//const string baseUrl = "http://mvpstream.azurewebsites.net/";
+			//var model = HomeService.GetModel(_searchService);
+			//var TextWriter = new XmlTextWriter(stream, Encoding.UTF8);
+			//TextWriter.WriteStartDocument();
+
+			////Below tags are mandatory rss tag
+			//TextWriter.WriteStartElement("rss");
+			//TextWriter.WriteAttributeString("version", "2.0");
+
+			//// Channel tag will contain RSS feed details
+			//TextWriter.WriteStartElement("channel");
+			//TextWriter.WriteElementString("title", "MVPStream");
+			//TextWriter.WriteElementString("description", "MVPStream RSS Feed");
+			//TextWriter.WriteElementString("link", baseUrl);
+
+			//foreach(var item in model.UltimasPublicaciones)
+			//{
+			//    TextWriter.WriteStartElement("item");
+			//    TextWriter.WriteElementString("title", item.Titulo);
+			//    TextWriter.WriteElementString("description", item.Descripcion);
+			//    TextWriter.WriteElementString("link", item.Url);
+			//    foreach(var tag in item.Tags){
+			//        TextWriter.WriteElementString("category", tag);
+			//    }
+			//    TextWriter.WriteEndElement();
+			//}
+			//foreach(var item in model.UltimosVideos)
+			//{
+			//    TextWriter.WriteStartElement("item");
+			//    TextWriter.WriteElementString("title", item.Titulo);
+			//    TextWriter.WriteElementString("description", item.Descripcion);
+			//    TextWriter.WriteElementString("link", item.Url);
+			//    foreach(var tag in item.Tags){
+			//        TextWriter.WriteElementString("category", tag);
+			//    }
+			//    TextWriter.WriteEndElement();
+			//}
+			//TextWriter.WriteEndElement();
+			//TextWriter.WriteEndElement();
+			//TextWriter.WriteEndDocument();
+			//TextWriter.Flush();
+			//stream.Position=0;
+			//using(var streamReader = new System.IO.StreamReader(stream))
+			//{
+
+			//    return Content(streamReader.ReadToEnd(), "application/rss+xml");    
+			//}
+			//}
+			return NoContent();
             
         }       
     }
